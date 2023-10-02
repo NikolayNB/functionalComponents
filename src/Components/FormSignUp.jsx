84a945f62bc7e2723e6817da571bebaaeddac99e
Nikolay Núñez <nikolaynunezballadares@gmail.com>
2023-10-02 22:54:27 +0200

    module 4 completed

--- src/Components/FormSignUp.jsx
+++ src/Components/FormSignUp.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField';
 import Switch from '@mui/material/Switch';
@@ -7,18 +7,21 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 
 function FormSignUp (){
 
-    const [name, setName] = useState('NIKOLAY');
-    useEffect(() => {console.log("cambio ", name)} , [name]);
+    const [name, setName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [promos, setPromos] = useState(true);
+    const [news, setNews] = useState(false);
 
     return (
-        <form>  
+        <form onSubmit={(e) => e.preventDefault()}>  
             <TextField 
                 id="name" 
                 label="Nombre" 
                 variant="outlined" 
                 fullWidth 
                 margin='normal'
-                onChange={(e) => {console.log(e.target.value); setName(e.target.value)}}
+                onChange={(e) =>  setName(e.target.value)}
                 value={name}
             />
             <TextField 
@@ -27,6 +30,8 @@ function FormSignUp (){
                 variant="outlined" 
                 fullWidth 
                 margin='normal'
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
             />
             <TextField 
                 id="email" 
@@ -34,19 +39,33 @@ function FormSignUp (){
                 variant="outlined" 
                 fullWidth 
                 margin='normal'
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
             />
 
             <FormGroup>
                 <FormControlLabel 
-                    control={<Switch id="promos" label="Promociones" defaultChecked />} 
+                    control={
+                        <Switch 
+                            id="promos" 
+                            label="Promociones" 
+                            checked={promos} 
+                            onchange={(e) => setPromos(e.target.checked)}
+                        />} 
                     label="Promociones" 
                 />
                 <FormControlLabel 
-                    control={<Switch id="news" label="Novedades" defaultChecked />} 
+                    control={
+                        <Switch 
+                            id="news" 
+                            label="Novedades" 
+                            checked={news}
+                            onchange={(e) => setNews(e.target.checked)}
+                        />} 
                     label="Novedades" 
                 />
             </FormGroup>
-            <Button variant='contained'>Registrarse</Button>
+            <Button variant='contained' type='submit'>Registrarse</Button>
         </form>
     )
 }
