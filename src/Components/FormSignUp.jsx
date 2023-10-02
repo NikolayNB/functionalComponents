05a9131a6b9b0c8e5e3cae2edd0bf0b0db795a53
Nikolay Núñez <nikolaynunezballadares@gmail.com>
2023-10-02 14:28:59 +0200

    Class 3.2 obtaining input value

--- src/Components/FormSignUp.jsx
+++ src/Components/FormSignUp.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField';
 import Switch from '@mui/material/Switch';
@@ -5,14 +6,44 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 
 function FormSignUp (){
+
+    const [name, setName] = useState('NIKOLAY');
+
     return (
         <form>  
-            <TextField id="name" label="Nombre" variant="outlined" fullWidth margin='normal'/>
-            <TextField id="surname" label="Apellido" variant="outlined" fullWidth margin='normal'/>
-            <TextField id="email" label="Email" variant="outlined" fullWidth margin='normal'/>
+            <TextField 
+                id="name" 
+                label="Nombre" 
+                variant="outlined" 
+                fullWidth 
+                margin='normal'
+                onChange={(e) => {console.log(e.target.value); setName(e.target.value)}}
+                value={name}
+            />
+            <TextField 
+                id="surname" 
+                label="Apellido" 
+                variant="outlined" 
+                fullWidth 
+                margin='normal'
+            />
+            <TextField 
+                id="email" 
+                label="Email" 
+                variant="outlined" 
+                fullWidth 
+                margin='normal'
+            />
+
             <FormGroup>
-                <FormControlLabel control={<Switch id="promos" label="Promociones" defaultChecked />} label="Promociones" />
-                <FormControlLabel control={<Switch id="news" label="Novedades" defaultChecked />} label="Novedades" />
+                <FormControlLabel 
+                    control={<Switch id="promos" label="Promociones" defaultChecked />} 
+                    label="Promociones" 
+                />
+                <FormControlLabel 
+                    control={<Switch id="news" label="Novedades" defaultChecked />} 
+                    label="Novedades" 
+                />
             </FormGroup>
             <Button variant='contained'>Registrarse</Button>
         </form>
